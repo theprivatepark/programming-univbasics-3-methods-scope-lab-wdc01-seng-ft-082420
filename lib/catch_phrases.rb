489c154f1fc04dbe2fb5ea3188750ed0d3330e3c
Anney Park <anneypark@gmail.com>
2020-07-20 00:56:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,17 +1,19 @@
 def mario
-  phrase = "It's-a me, Mario!"
-  puts phrase
-  phrase
+  all_phrases = "It's-a me, Mario!"
+  puts all_phrases
+  all_phrases
 end
 
 def toadstool
-    status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-    puts = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts status
+  all_phrases = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts all_phrases
+  all_phrases
 end
 
 def link 
-  'It's Dangerous To Go Alone! Take This.'
+  all_phrases = "'It's Dangerous To Go Alone! Take This.'"
+  puts all_phrases
+  all_phrases
 end
 
 def all_phrases
