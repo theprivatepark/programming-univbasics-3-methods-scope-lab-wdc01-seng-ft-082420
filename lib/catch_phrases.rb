2f5cf67835f4468bee43f5b10e34ff24a7073a79
Anney Park <anneypark@gmail.com>
2020-07-20 00:37:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,6 +7,7 @@ end
 
 def toadstool
     status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+    puts = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
